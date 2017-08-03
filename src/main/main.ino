61339c483d21dc14fc683aa888c1bb2cf290804b
alexzulu <ua6hdw@gmail.com>
2017-08-03 12:16:37 +0300

    Правка логики. Правка схемы для симуляции.

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,12 +1,18 @@
 int switches[5] = {4,5,6,7,8,};
 int machines[3] = {9,10,11};
 int jogs[15] = {0,1,1,2,0,1,0,2,1,0,1,2,1,0,2};
-int offset_1 = 5000;
+int offset_1 = 2000;
 int currentOffset_1;
 int steps;
+boolean work = false;
+boolean pause = false;
+
+#define START 2
 
 void setup() {
   Serial.begin(9600);
+  attachInterrupt(0, modes, RISING);
+  pinMode(START, INPUT);
   for(int count = 0; count < 5; count++){
    pinMode(switches[count], OUTPUT); 
   }
@@ -16,5 +22,13 @@ void setup() {
 }
 
 void loop() {
-  order();
+  if(debounce(digitalRead(START)) && work == false){
+    work = true;
+  }
+  Serial.println(work);
+  if(work == true){
+    order();
+  }
+  delay(150);
+  work = false;
 }
--- src/main/order.ino
+++ src/main/order.ino
@@ -1,9 +1,5 @@
 void order(){
   steps =0;
-  Serial.print("Steps = ");
-  Serial.print(steps);
-  Serial.print(" Machine = ");
-  Serial.println(jogs[steps]);
   while(steps < 15){
     order_2();
   }
@@ -12,26 +8,34 @@ void order(){
 void order_2(){
   int i = 0;
   while(i < 5){
-    currentOffset_1 = 0;
-    int start = millis();
-    while (currentOffset_1 < offset_1){
-      if(!digitalRead(switches[i])){
-        digitalWrite(switches[i],HIGH);
+    if(pause == false){
+      Serial.print("Steps = ");
+      Serial.print(steps);
+      Serial.print(" Pause = ");
+      Serial.print(pause);
+      Serial.print(" Machine = ");
+      Serial.println(jogs[steps]);
+      currentOffset_1 = 0;
+      int start = millis();
+      while (currentOffset_1 < offset_1){
+        if(pause == false){
+           if(!digitalRead(switches[i])){
+            digitalWrite(switches[i],HIGH);
+          }
+          if(!digitalRead(machines[jogs[steps]])){
+            digitalWrite(machines[jogs[steps]],HIGH);
+          }
+//        delay(50);
+          currentOffset_1 = millis() - start;
+        }
       }
-      if(!digitalRead(machines[jogs[steps]])){
-        digitalWrite(machines[jogs[steps]],HIGH);
+      digitalWrite(machines[jogs[steps]],LOW);
+      digitalWrite(switches[i],LOW);
+      if(pause == false){
+        steps++;
+        i++;
       }
-      delay(50);
-      currentOffset_1 = millis() - start;
     }
-    digitalWrite(machines[jogs[steps]],LOW);
-    digitalWrite(switches[i],LOW);
-    steps++;
-    i++;
-    Serial.print("Steps = ");
-    Serial.print(steps);
-    Serial.print(" Machine = ");
-    Serial.println(jogs[steps]);
   }
 }
 
